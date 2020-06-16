e6a1339460169d2cb89d8bf35536752de02aeb18
tony-tsx <tony.js@zoho.eu>
2020-06-16 11:57:20 -0300

    program by location

--- src/routes/api/programs.ts
+++ src/routes/api/programs.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from 'axios'
 import Express from 'express'
 
 import wpApi from '../../helpers/wp-api'
@@ -5,7 +6,20 @@ import wpApi from '../../helpers/wp-api'
 const programs = Express.Router()
 
 programs.get( '/programs', async ( req, res ) => {
-  const response = await wpApi.get( '/ndmais/v1/content/filters/program' )
+  const locations: string[] = req.query?.locations?.toString()
+    .split( ',' ) || []
+
+  let promise: Promise<AxiosResponse>
+
+  if ( locations.length )
+
+    if ( locations.length > 1 ) return res.status( 422 ).json()
+
+    else promise = wpApi.get( `/ndmais/v1/content/filters/program/location/${locations[0]}` )
+
+  else promise = wpApi.get( '/ndmais/v1/content/filters/program'  )
+
+  const response = await promise
 
   res.json( response.data )
 } )
