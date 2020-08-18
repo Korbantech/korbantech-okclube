872adf4efc000705d8422a9acdb5efb8ba844592
Tony Tsx <tony.js@zoho.eu>
2020-08-18 17:53:44 -0300

    script to populate polls

--- src/message-test.ts
+++ src/message-test.ts
@@ -1,23 +1,17 @@
-import firebase from './helpers/firebase';
+import connection from './helpers/connection'
 
 ( async () => {
-  const collections = await firebase.firestore().collection( 'users' ).get()
-  let users: any[] = []
-  collections.forEach( user => users.push( user.data() ) )
-
-  const iosDevices = users.filter( user => user.platform === 'ios' )
-
-  const tokens = iosDevices.map( user => user.token )
-  const response = await firebase.messaging().sendToDevice( tokens, {
-    notification: {
-      title: 'Teste para ios',
-      body: 'Apenas um teste para ios'
-    }
+  await connection( 'polls' ).insert( {
+    text: 'Teste para Grande Florianópolis',
+    location: 'grande-florianopolis'
   } )
-  response.results.forEach( result => {
-    console.log( result )
-    if ( result.error )
-      console.error( `Error: ${result.error.message}(${result.error.code})` )
+  await connection( 'polls' ).insert( {
+    text: 'Teste para Blumenau e região',
+    location: 'blumenau-e-regiao"'
   } )
-  process.exit()
+  await connection( 'polls' ).insert( {
+    text: 'Teste para Joinville e região',
+    location: 'norte'
+  } )
+  connection.destroy()
 } )()
