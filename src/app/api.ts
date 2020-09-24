ec2a5a66f4c12d16283fdb77d05b64c7f404af86
Tony Tsx <tony.js@zoho.eu>
2020-09-24 17:50:58 -0300

    update host

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,6 +1,8 @@
 import Axios from 'axios'
 
-const api = Axios.create( { baseURL: 'http://localhost:9000/api' } )
+const api = Axios.create( {
+  baseURL: false ? 'http://localhost:9000/api' : 'http://api.app.ndmais.com.br/'
+} )
 
 api.interceptors.request.use( request => {
   return request
