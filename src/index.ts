bbaeb9d06444dd33bc9a090989c14df4d773040b
Tony Tsx <tony.js@zoho.eu>
2020-09-25 18:52:11 -0300

    fix: to many redirects

--- src/index.ts
+++ src/index.ts
@@ -40,8 +40,9 @@ app.set( 'port', args.port ?? 80 )
 
 app.use( morgan( app.get( 'log type' ), IS_PRODUCTION_ENVIRONMENT ? { stream } : undefined ) )
 app.use( ( req, res, next ) => {
+  res.render( 'index.pug' )
   if ( req.protocol === 'http' && IS_PRODUCTION_ENVIRONMENT )
-    res.redirect( `https://${req.hostname}/${req.originalUrl}`, 308 )
+    res.redirect( `https://${req.hostname}${req.originalUrl}`, 308 )
   else next()
 } )
 app.use( cors() )
