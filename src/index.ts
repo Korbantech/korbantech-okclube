32a8170f74158a07c24cf4887584d2f451b74621
Tony Tsx <tony.js@zoho.eu>
2020-09-25 18:57:57 -0300

    debug: header

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ app.set( 'port', args.port ?? 80 )
 
 app.use( morgan( app.get( 'log type' ), IS_PRODUCTION_ENVIRONMENT ? { stream } : undefined ) )
 app.use( ( req, res, next ) => {
-  console.log( req.protocol )
+  console.log( req.protocol, req.headers )
   if ( req.protocol === 'http' && IS_PRODUCTION_ENVIRONMENT )
     return res.redirect( 308, `https://${req.hostname}${req.originalUrl}` )
   else next()
